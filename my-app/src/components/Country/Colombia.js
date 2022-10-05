de25a9e22367efd25070c201027c4c5fde02d4d9
Alan Naranjo <85073731+L-A-M-J-O@users.noreply.github.com>
2022-10-05 17:25:14 +0200

    Cambios

--- my-app/src/components/Country/Colombia.js
+++ my-app/src/components/Country/Colombia.js
@@ -1,53 +1,10 @@
 
-
-import { useEffect, useState } from 'react';
-import getApi from '../helpers/getApi';
+import { useState, useEffect } from 'react'
 
 
 function Colombia() {
-
-    const [data, setData] = useState([])
-    
-
-    const updateApi = () => {
-        getApi()
-            .then((newCharacter) => {
-                setData(newCharacter);
-            })
-    }
-
-    useEffect( () => {
-        updateApi();
-    }, [])
-
-
-    // const filter = (pais) => {
-    //     const result = data.filter((element) => {
-    //         return element.category === pais
-    //     })
-    //     setData(result)
-    // }
-
-  return ( 
-    <div className='container'>
-    {
-        data.map(element => (
-            <div className='row' key={element.id}>
-                <div className='col-10'>
-                    <h4>{element.name}</h4>
-                </div>
-            </div>
-        ))
-    }
-
-        {/* <div className='container'>
-            <div className='row justify-content-center'>
-                <div className='col-6 border'>
-                    <button onClick={ () => filter('Belgica')}>Cambio</button>
-                </div>
-            </div>
-        </div> */}
-    </div>
+  return (
+    <div>Colombia</div>
   )
 }
 

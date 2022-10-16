9145ebebb4024d4da88a3bcd40e2149ade1ba211
L-A-M-J-O <lamjo30@gmail.com>
2022-10-16 03:29:30 +0200

    Cambios

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,20 +1,30 @@
+import { useState } from "react";
 import "../Form/Form.css";
 
 export default function Form() {
+
+	const [email, setEmail] = useState();
+	const [password, setPassword] = useState();
+
+	const handleSubmit = (event => {
+		event.preventDefault();
+		const gEmail = password;
+		const gPassword = password;
+		console.log(gEmail, gPassword)
+	})
+
 	return (
 		<div className="container">
 			<div className="row justify-content-center">
-				<div className="col-6 mt-5">
-					<form className="p-5 m-3 form-main">
+				<div className="col-12 col-sm-12 col-md-6 col-lg-6 mt-5">
+					<form className="p-5 m-3 form-main" onChange={handleSubmit}>
 						<div className="mb-3">
 							<label for="exampleInputEmail1" className="form-label">
 								Name
 							</label>
 							<input
-								type="email"
+								type="name"
 								className="form-control rounded-pill"
-								id="exampleInputEmail1"
-								aria-describedby="emailHelp"
 							/>
 						</div>
 						<div className="mb-3">
@@ -22,10 +32,8 @@ export default function Form() {
 								LatsName
 							</label>
 							<input
-								type="email"
+								type="lastname"
 								className="form-control rounded-pill"
-								id="exampleInputEmail1"
-								aria-describedby="emailHelp"
 							/>
 						</div>
 						<div className="mb-3">
@@ -37,6 +45,10 @@ export default function Form() {
 								className="form-control rounded-pill"
 								id="exampleInputEmail1"
 								aria-describedby="emailHelp"
+								value={email}
+								onChange={(
+									event,
+								) => setEmail(event.target.value)}
 							/>
 							<div id="emailHelp" className="form-text">
 								We'll never share your email with anyone else.
@@ -50,54 +62,21 @@ export default function Form() {
 								type="password"
 								className="form-control rounded-pill"
 								id="exampleInputPassword1"
+								onChange={(
+									event,
+								) => setPassword(event.target.value)}
 							/>
 						</div>
 						<div className="row justify-content-center p-2">
-							<div className="col-4 p-2">
-								<div className="mb-3 form-check">
-									<input
-										type="checkbox"
-										className="form-check-input"
-										id="exampleCheck1"
-									/>
-									<label className="form-check-label" for="exampleCheck1">
-										Check me out
-									</label>
+							<div className="col-12 col-sm-12 col-md-4 col-lg-4	p-2">
+								<div className="col-12 text-center">
+									<button type="submit" className="btn btn-primary">
+										Confirm Buy
+									</button>
 								</div>
-							</div>
-							<div className="col-3 p-0 m-0">
-								<button type="submit" class="btn btn-primary">
-									Confirm Buy
-								</button>
 							</div>
 						</div>
 					</form>
-				</div>
-				<div className="col-6 mt-5">
-					<div className="row m-2 justify-content-center">
-						<div className="col-2">
-							<h4>Beer</h4>
-						</div>
-						<div className="col-12">
-							<div className="row justify-content-center p-2 m-2">
-								<div className="col-8">
-									<div className="m-card-m col-12 col-sm-4 col-md-2 col-lg-2">
-										<div className="m-card row gx-0 gy-0 mt-3">
-											<img className="m-card-img" src="" alt="item imagen" />
-										</div>
-										<div className="row justify-content-center">
-											<div className="col-9 mt-4">
-												<p className="name">a</p>
-											</div>
-											<div className="col-4">
-												<p>a</p>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
 				</div>
 			</div>
 		</div>

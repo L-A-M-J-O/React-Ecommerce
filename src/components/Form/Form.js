7e58c8b5d8b026cd84949fd7e3cbe2f25ec3d496
L-A-M-J-O <lamjo30@gmail.com>
2022-10-20 22:37:10 +0200

    Finish

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,84 +1,89 @@
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "../Form/Form.css";
+import { Link } from 'react-router-dom'
 
 export default function Form() {
+	const [modal, setModal] = useState(false);
+	const [form, setForm] = useState(true);
 
+		const toggleModal = () => {
+			setModal(true)
+			setForm(false)
+		}
+	
 	const [email, setEmail] = useState();
 	const [password, setPassword] = useState();
 
 	const handleSubmit = (event => {
 		event.preventDefault();
-		const gEmail = password;
-		const gPassword = password;
-		console.log(gEmail, gPassword)
 	})
-
 	return (
-		<div className="container">
-			<div className="row justify-content-center">
-				<div className="col-12 col-sm-12 col-md-6 col-lg-6 mt-5">
-					<form className="p-5 m-3 form-main" onChange={handleSubmit}>
-						<div className="mb-3">
-							<label for="exampleInputEmail1" className="form-label">
-								Name
-							</label>
-							<input
-								type="name"
-								className="form-control rounded-pill"
-							/>
-						</div>
-						<div className="mb-3">
-							<label for="exampleInputEmail1" className="form-label">
-								LatsName
-							</label>
-							<input
-								type="lastname"
-								className="form-control rounded-pill"
-							/>
-						</div>
-						<div className="mb-3">
-							<label for="exampleInputEmail1" className="form-label">
-								Email address
-							</label>
-							<input
-								type="email"
-								className="form-control rounded-pill"
-								id="exampleInputEmail1"
-								aria-describedby="emailHelp"
-								value={email}
-								onChange={(
-									event,
-								) => setEmail(event.target.value)}
-							/>
-							<div id="emailHelp" className="form-text">
-								We'll never share your email with anyone else.
+		<>
+		{form && (
+				<div className="container">
+				<div className="row justify-content-center">
+					<div className="col-12 col-sm-12 col-md-6 col-lg-6 mt-5">
+						<form className="p-5 m-3 form-main" onChange={handleSubmit}>
+							<div className="mb-3">
+								<label for="exampleInputEmail1" className="form-label">
+									Email address
+								</label>
+								<input
+									type="email"
+									className="form-control rounded-pill"
+									id="exampleInputEmail1"
+									aria-describedby="emailHelp"
+									value={email}
+									onChange={(e) => {
+										setEmail(e.target.value)
+									}}
+								/>
+								<div id="emailHelp" className="form-text">
+									We'll never share your email with anyone else.
+								</div>
 							</div>
-						</div>
-						<div className="mb-3">
-							<label for="exampleInputPassword1" className="form-label">
-								Password
-							</label>
-							<input
-								type="password"
-								className="form-control rounded-pill"
-								id="exampleInputPassword1"
-								onChange={(
-									event,
-								) => setPassword(event.target.value)}
-							/>
-						</div>
-						<div className="row justify-content-center p-2">
-							<div className="col-12 col-sm-12 col-md-4 col-lg-4	p-2">
-								<div className="col-12 text-center">
-									<button type="submit" className="btn btn-primary">
+							<div className="mb-3">
+								<label for="exampleInputPassword1" className="form-label">
+									Password
+								</label>
+								<input
+									type="password"
+									className="form-control rounded-pill"
+									id="exampleInputPassword1"
+									value={password}
+									onChange={(e) => {
+										setPassword(e.target.value)
+									}}
+								/>
+							</div>
+		
+						</form>
+						<div className="row justify-content-center">
+							<div className="col-4">
+								<div className="row">
+									<button	button className="btn btn-primary" onClick={toggleModal}>
 										Confirm Buy
 									</button>
 								</div>
 							</div>
 						</div>
-					</form>
+					</div>
 				</div>
 			</div>
+		)} 
+		{modal && (
+			<div className='row row-cols-1 p-3 justify-content-center box-modal mt-5'>
+				<div className='col-12 text-center'>
+					<h2>Muchas Gracias por tu compra</h2>
+				</div>
+				<div className='col-4 text-center mt-3'>
+					<Link to='/'>
+						<button className="btn btn-primary">Aceptar</button>
+					</Link>
+				</div>
 		</div>
+		)}
+		</>
 	);
 }
